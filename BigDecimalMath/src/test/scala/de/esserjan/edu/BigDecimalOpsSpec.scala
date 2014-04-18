811f65d7e49e7f07a268b97250152d5973a936ec
Jan Esser <jan@esserjan.de>
2014-04-18 10:37:10 +0200

    BigDecimalOpsSpec simplified

--- BigDecimalMath/src/test/scala/de/esserjan/edu/BigDecimalOpsSpec.scala
+++ BigDecimalMath/src/test/scala/de/esserjan/edu/BigDecimalOpsSpec.scala
@@ -6,61 +6,36 @@ import org.scalatest.prop.PropertyChecks
 
 import BigDecimalMath._
 
-object BigDecimalOpsSpec {
-  case class Square(val d: BigDecimal) {
-    val sq = d * d
-  }
-  /**
-   * Range of squares (with positive roots).
-   */
-  val squares = for (d <- Gen.choose(1.0, 1e32)) yield Square(d)
-
-  /**
-   * Generic range of positive and negative BigDecimals.
-   */
-  val bigDecimals = for (n <- Gen.choose(-1e64, 1e64)) yield BigDecimal(n)
-}
-
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class BigDecimalOpsSpec extends FlatSpec with Matchers with PropertyChecks {
-  import BigDecimalOpsSpec._
 
-  "BigDecimalMath - BigDecimalOps" should "root squares" in {
-    forAll(squares) { (sq: Square) =>
-      sq.sq.sqrt should be(sq.d)
-    }
+  "BigDecimalMath - BigDecimalOps" should "square-root big decimals" in {
+    (s: BigDecimal) =>
+      try {
+        val square = s * s
+        (square.sqrt - s.abs).abs should be <= square.ulp
+      } catch {
+        case ex: ArithmeticException =>
+          println(s"Test omitted because ($s pow 2) caused ArithmeticException: " + ex.getMessage)
+        case ex: NumberFormatException =>
+          println(s"Test omitted because ($s pow 2) caused NumberFormatException: " + ex.getMessage)
+      }
   }
 
-  def testSquare = (s: BigDecimal) =>
-    try {
-      (s * s).sqrt should be(s.abs +- s.ulp)
-    } catch {
-      case ex: ArithmeticException =>
-        println(s"Test omitted because ($s pow 2) caused ArithmeticException: " + ex.getMessage)
-      case ex: NumberFormatException =>
-        println(s"Test omitted because ($s pow 2) caused NumberFormatException: " + ex.getMessage)
-    }
-
-  it should "square-root some bigDecimals" in {
-    forAll(bigDecimals)(testSquare)
-  }
-
-  it should "square-root +-1E150" in {
-    forAll(for (n <- Gen.choose(-1e150, 1e150)) yield BigDecimal(n))(testSquare)
-  }
-
-  it should "square-root [0,1E-150]" in {
-    forAll(for (n <- Gen.choose(0.0, 1e-150)) yield BigDecimal(n))(testSquare)
-  }
-
-  it should "cubic-root some bigDecimals" in {
-    forAll(bigDecimals) {
-      (s: BigDecimal) =>
-        (s * s * s).cqrt should be(s +- s.ulp)
-    }
+  it should "cubic-root big decimals" in {
+    (s: BigDecimal) =>
+      try {
+        val cubic = s * s * s
+        (cubic.cqrt.abs - s.abs).abs should be <= cubic.ulp
+      } catch {
+        case ex: ArithmeticException =>
+          println(s"Test omitted because ($s pow 2) caused ArithmeticException: " + ex.getMessage)
+        case ex: NumberFormatException =>
+          println(s"Test omitted because ($s pow 2) caused NumberFormatException: " + ex.getMessage)
+      }
   }
 
   it should "negate" in {
@@ -79,7 +54,7 @@ class BigDecimalOpsSpec extends FlatSpec with Matchers with PropertyChecks {
     import BigIntOpsSpec.faculties
     forAll(faculties) {
       (n: Int, fac: Int) =>
-        BigDecimal(n).factorial.intValue should be(fac)
+        BigDecimal(n).setScale(5).factorial should be(fac)
     }
   }
 
@@ -90,11 +65,6 @@ class BigDecimalOpsSpec extends FlatSpec with Matchers with PropertyChecks {
   }
 
   it should "stripTrailingZeroes" in {
-    forAll(bigDecimals) {
-      (d: BigDecimal) =>
-        val s = d.setScale(d.scale + 3)
-
-        s.stripTrailingZeros.scale should be(d.scale)
-    }
+    BigDecimal("1.000").stripTrailingZeros.scale should be(0)
   }
 }
